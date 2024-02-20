4877cf87d469d4c91f58d6baa462d56042a8697b
Agus <agusptr44@gmail.com>
2024-02-20 21:47:33 +0700

    class and object

--- ClassObject/Object_mobil.php
+++ ClassObject/Object_mobil.php
@@ -1,9 +1,9 @@
 <?php
 include("Class_mobil.php");
 
-// Membuat objek Mobil_Pickup
+// Membuat objek Mobil_pickup
 $obj_mobil_pickup_merah = new Mobil_Pickup();
-$obj_mobil_pickup_biru = new Mobil_Pickup();
+$obj_mobil_pickup_biru = new Mobil_pickup();
 
 $obj_mobil_pickup_merah->set_merk("Carry");
 $obj_mobil_pickup_merah->set_jenis_bbm("Bensin");
@@ -15,6 +15,7 @@ $obj_mobil_pickup_merah->set_berat_max(200);
 $obj_mobil_pickup_merah->set_max_bbm(45);
 $obj_mobil_pickup_merah->set_konsumsi_bbm(15);
 $obj_mobil_pickup_merah->set_isi_bbm(10);
+
 
 $obj_mobil_pickup_biru->set_merk("Grand max");
 $obj_mobil_pickup_biru->set_jenis_bbm("Solar");
@@ -28,43 +29,68 @@ $obj_mobil_pickup_biru->set_konsumsi_bbm(10);
 $obj_mobil_pickup_biru->set_isi_bbm(10);
 
 
-// tampilkan informasi mobil carry
+echo "Merk mobil merah adalah " . $obj_mobil_pickup_merah->get_merk() . "<br>";
+echo "Jenis BBM mobil merah adalah " . $obj_mobil_pickup_merah->get_jenis_bbm() . "<br>";
+echo "Silinder mobil merah adalah " . $obj_mobil_pickup_merah->get_isi_silinder() . "<br>";
+echo "Lebar bak mobil merah adalah " . $obj_mobil_pickup_merah->get_l_bak() . "<br>";
+echo "Panjang bak mobil merah adalah " . $obj_mobil_pickup_merah->get_p_bak() . "<br>";
+echo "Tinggi bak mobil merah adalah " . $obj_mobil_pickup_merah->get_t_bak() . "<br>";
+echo "Berat max mobil merah adalah " . $obj_mobil_pickup_merah->get_berat_max() . "<br>";
+echo "<br>";
+
+
+
+echo "<br>";
+echo "Merk mobil biru adalah " . $obj_mobil_pickup_biru->get_merk() . "<br>";
+echo "Jenis BBM mobil biru adalah " . $obj_mobil_pickup_biru->get_jenis_bbm() . "<br>";
+echo "Silinder mobil biru adalah " . $obj_mobil_pickup_biru->get_isi_silinder() . "<br>";
+echo "Lebar bak mobil biru adalah " . $obj_mobil_pickup_biru->get_l_bak() . "<br>";
+echo "Panjang bak mobil biru adalah " . $obj_mobil_pickup_biru->get_p_bak() . "<br>";
+echo "Tinggi bak mobil biru adalah " . $obj_mobil_pickup_biru->get_t_bak() . "<br>";
+echo "Berat max mobil biru adalah " . $obj_mobil_pickup_biru->get_berat_max() . "<br>";
+echo "<br>";
+
+
+
+// Informasi mobil carry
 echo "Mobil: " . $obj_mobil_pickup_merah->get_merk() . "<br>";
-echo "Tampilkan nambah berat" . "<br>";
-$obj_mobil_pickup_merah->isi_pasir(1);
+echo "Tampilkan Muatan berat" . "<br>";
+
+echo "Muatan ditambah: ";
+$obj_mobil_pickup_merah->isi_pasir(15);
+echo "Muatan ditambah: ";
 $obj_mobil_pickup_merah->isi_pasir(3);
-$obj_mobil_pickup_merah->isi_pasir(10);
-$obj_mobil_pickup_merah->isi_barang(50);
-echo "Total volume bak tersedia: " . $obj_mobil_pickup_merah->get_vol_diangkut() . "<br>";
-echo "Berat Diangkut: " . $obj_mobil_pickup_merah->get_berat_diangkut() . "<br>";
-echo "Jumlah bbm saat ini: " . $obj_mobil_pickup_merah->get_sisa_bbm() . "l <br>";
+echo "Muatan ditambah: ";
+$obj_mobil_pickup_merah->isi_barang(10);
+echo "<br>";
+echo "Total volume bak yang tersedia: " . $obj_mobil_pickup_merah->get_vol_diangkut() . "<br>";
+echo "Berat muatan mobil carry: " . $obj_mobil_pickup_merah->get_berat_diangkut() . "<br>";
+echo "Jumlah sisa bbm saat ini: " . $obj_mobil_pickup_merah->get_sisa_bbm() . "<br>";
 $sisa_bbm_pickup_merah = $obj_mobil_pickup_merah->get_sisa_bbm();
 $obj_mobil_pickup_merah->set_jarak(330);
-echo "Hitung uang yang harus diberi ke supir untuk menempuh jarak " . $obj_mobil_pickup_merah->get_jarak() . "km yogyakarta-surabaya pulang pergi adalah: " . $obj_mobil_pickup_merah->hitung_bayaran($sisa_bbm_pickup_merah) * 2 . "<br>";
-// echo"Jarak yang di tempuh oleh mobil ".$obj_mobil_pickup_merah." adalah : ".$obj_mobil_pickup_merah->get_jarak()."km <br>";
-
-
-
-
-
+echo "Uang yang harus diberi ke supir untuk menempuh jarak " . $obj_mobil_pickup_merah->get_jarak() . " KM, yogyakarta-surabaya pulang pergi adalah: " . $obj_mobil_pickup_merah->hitung_bayaran($sisa_bbm_pickup_merah) * 2 . "<br>";
 
 
 echo "<br><br>";
-// tampilkan informasi mobil grand max
+// Informasi mobil grand max
 echo "Mobil: " . $obj_mobil_pickup_biru->get_merk() . "<br>";
 echo "Tampilkan nambah berat" . "<br>";
-$obj_mobil_pickup_biru->isi_pasir(1);
+echo "Muatan ditambah: ";
 $obj_mobil_pickup_biru->isi_barang(50);
-$obj_mobil_pickup_biru->isi_pasir(3);
-$obj_mobil_pickup_biru->isi_pasir(10);
+echo "Muatan ditambah: ";
+$obj_mobil_pickup_biru->isi_barang(65);
+echo "Muatan ditambah: ";
+$obj_mobil_pickup_biru->isi_barang(70);
+echo "Muatan ditambah: ";
+$obj_mobil_pickup_biru->isi_barang(80);
+echo "<br>";
 echo "Total volume bak tersedia: " . $obj_mobil_pickup_biru->get_vol_diangkut() . "<br>";
 echo "Berat Diangkut: " . $obj_mobil_pickup_biru->get_berat_diangkut() . "<br>";
-echo "Jumlah bbm saat ini: " . $obj_mobil_pickup_biru->get_sisa_bbm() . "l <br>";
+echo "Jumlah bbm saat ini: " . $obj_mobil_pickup_biru->get_sisa_bbm() . " <br>";
 $sisa_bbm_pickup_biru = $obj_mobil_pickup_biru->get_sisa_bbm();
-echo "Jumlah bbm saat ini setelah menempuh jarak: " . $obj_mobil_pickup_biru->get_jarak() . "km adalah " . $obj_mobil_pickup_biru->get_sisa_bbm() . "l <br>";
 $obj_mobil_pickup_biru->set_jarak(330);
-echo "Hitung uang yang harus diberi ke supir untuk menempuh jarak " . $obj_mobil_pickup_biru->get_jarak() . "km yogyakarta-surabaya pulang pergi adalah: " . $obj_mobil_pickup_biru->hitung_bayaran($sisa_bbm_pickup_biru) * 2 . "<br>";
-
+echo "Jumlah bbm saat ini setelah menempuh jarak: " . $obj_mobil_pickup_biru->get_jarak() . " KM adalah " . $obj_mobil_pickup_biru->get_sisa_bbm() . " <br>";
+echo "Uang yang harus diberi ke supir untuk menempuh jarak " . $obj_mobil_pickup_biru->get_jarak() . " KM, yogyakarta-surabaya adalah: " . $obj_mobil_pickup_biru->hitung_bayaran($sisa_bbm_pickup_biru) * 2 . "<br>";
 
 echo "<br><br>";
 $total_biaya_pickup_merah = $obj_mobil_pickup_merah->hitung_bayaran($sisa_bbm_pickup_merah);
